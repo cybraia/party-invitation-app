1e7d6f38c74d908d09413783ea4c6a2da5aca7fa
cybraia <27krishna2002@gmail.com>
2021-11-18 14:40:51 +0530

    added changes

--- project/src/App.js
+++ project/src/App.js
@@ -1,137 +1,145 @@
-import React from 'react';
-import { useState } from 'react';
-import './App.css';
-import {AiFillDelete} from 'react-icons/ai';
+import { useState } from "react";
+import "./App.css";
+// components
+import { AiFillDelete } from "react-icons/ai";
 import { FaEdit } from "react-icons/fa";
-
+//helper functions
+import { removeItemFromArr } from "./utils/name";
 
 function App() {
   const [name, setName] = useState("");
   const [num, setNum] = useState("");
   const [opt, setOpt] = useState("");
-  const [ waitList, setWaitList ] = useState([]);
+  const [waitList, setWaitList] = useState([]);
   const [confirmList, setConfirmList] = useState([]);
-  const [count,setCount] = useState(0);
-  const [countwait, setCountwait] = useState(0);
 
-  
   const handleSubmit = (e) => {
     e.preventDefault();
-    setWaitList([...waitList, {name: name, number: num, options: opt}]);
-    console.log(waitList);
-    setName('');setNum('');setOpt('');
-    setCountwait(countwait => countwait+1);
+    setWaitList([...waitList, { name: name, number: num, options: opt }]);
+    setName("");
+    setNum("");
+    setOpt("");
   };
-
-  const handleDelete = (index) =>{
-    var newList = waitList;
-    newList.splice(index,1);
-    setWaitList([...newList])
-    setCountwait(countwait => countwait - 1);
-  }
-
-  const handleDelete2 = (index) =>{
-    var newList = confirmList;
-    newList.splice(index,1);
-    setConfirmList([...newList])
-    setCount(count => count - 1);
-  }
 
   const handleAdd = (e) => {
     e.preventDefault();
-    setConfirmList([...confirmList,...waitList]);
+    setConfirmList([...confirmList, ...waitList]);
     setWaitList([]);
-    let c = countwait;
-    setCount(count => count + c);
-    setCountwait(0);
-  }
-
-  const handleEdit = () =>{
-    // const list = [...waitList];
-    // list[i].name = e.target.value;
-    // setWaitList(list); 
-  }
+  };
 
   return (
-    
-    <div className='main'>
-
-      <h1><span class="title">Invitation</span> App</h1>
+    <div className="main">
+      <h1>
+        <span class="title">Invitation</span> App
+      </h1>
       <form onSubmit={handleSubmit}>
-        <input type='text' placeholder='Enter Name' value={name} onChange={e => setName(e.target.value)} className='inputs' required /> 
-        <input type='text' placeholder='Enter Phone Number' pattern="[1-9]{1}[0-9]{9}" value={num} onChange={e => setNum(e.target.value)} className='inputs' required/>
-        <select className='inputs' value={opt} onChange={e => setOpt(e.target.value)} required>
+        <input
+          type="text"
+          placeholder="Enter Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          className="inputs"
+          required
+        />
+        <input
+          type="text"
+          placeholder="Enter Phone Number"
+          pattern="[1-9]{1}[0-9]{9}"
+          value={num}
+          onChange={(e) => setNum(e.target.value)}
+          className="inputs"
+          required
+        />
+        <select
+          className="inputs"
+          value={opt}
+          onChange={(e) => setOpt(e.target.value)}
+          required
+        >
           <option value="VVIP">VVIP</option>
           <option value="VIP">VIP</option>
           <option value="Special">Special</option>
           <option value="General">General</option>
         </select>
-        <button type='submit' className='add'>
+        <button type="submit" className="add">
           +
-       </button>
+        </button>
       </form>
 
-      <div> 
-        {waitList.length>0
-        ?
-        <div>
-        <h2>Wait List</h2>
-         {waitList.map((person,index) => 
-            <div key={index} className="list1">
-                <div>{person.name} - {person.number} - {person.options}
-                <FaEdit size={28} color="lime" onClick={(e,i) => handleEdit()} className='delete'></FaEdit>
-                <AiFillDelete size={30} color="red" onClick={() => handleDelete(index)} className='delete'/>
+      <div>
+        {waitList.length > 0 ? (
+          <div>
+            <h2>Wait List</h2>
+            {waitList.map((person, index) => (
+              <div key={index} className="list1">
+                <div>
+                  {person.name} - {person.number} - {person.options}
+                  <FaEdit
+                    size={28}
+                    color="lime"
+                    onClick={(e, i) => handleEdit()}
+                    className="delete"
+                  ></FaEdit>
+                  <AiFillDelete
+                    size={30}
+                    color="red"
+                    onClick={() => setWaitList(removeItemFromArr(index, waitList))}
+                    className="delete"
+                  />
                 </div>
-            </div>
-            )}
+              </div>
+            ))}
             <form onSubmit={handleAdd}>
               <input type="submit" value="Confirm" className="add" />
-            </form> 
+            </form>
             <br />
           </div>
-        :
-        <div>
-          <p>Add people to invite!</p>
-        </div>}
+        ) : (
+          <div>
+            <p>Add people to invite!</p>
+          </div>
+        )}
       </div>
-      
-        <div>
-          {confirmList.length>0 ? 
+
+      <div>
+        {confirmList.length > 0 ? (
+          <div>
+            <h2>Invited List {confirmList.length}</h2>
+
             <div>
-              <h2>Invited List {count}</h2>
-              
-                  <div>
-                  
-                    {/* <tr>
+              {/* <tr>
                       <th>Name</th>
                       <th>Number</th>
                       <th>Status</th>
                     </tr> */}
-                  {confirmList.map((person,index)=>(
-                  <div className="list1">
+              {confirmList.map((person, index) => (
+                <div className="list1">
                   <div key={index}>
-                  {/* <table>
+                    {/* <table>
                   <tr>
                   <td>{person.name}</td>
                   <td>{person.number}</td>
                   <td>{person.options}</td> */}
-                  {person.name} - {person.number} - {person.options}
-                  <AiFillDelete size={30} color="red" onClick={() => handleDelete2(index)} className='delete'/>
-                  {/* </tr>
+                    {person.name} - {person.number} - {person.options}
+                    <AiFillDelete
+                      size={30}
+                      color="red"
+                      onClick={() => setConfirmList(removeItemFromArr(index, confirmList))}
+                      className="delete"
+                    />
+                    {/* </tr>
                   </table> */}
                   </div>
-                  </div>
-                    ))}  
-              </div>
+                </div>
+              ))}
             </div>
-            :
-            <div>
-              {/* <p>Click on Confirm add to see the invite list</p> */}
-            </div>
-          }
-        </div>
+          </div>
+        ) : (
+          <div>{/* <p>Click on Confirm add to see the invite list</p> */}</div>
+        )}
       </div>
-    )
-} 
+    </div>
+  );
+}
 
-export default App ;
+export default App;
--- project/src/utils/name.js
+++ project/src/utils/name.js
@@ -1,18 +1,31 @@
-/**
- * 
- * @param {String} name name of the person
- * @returns greeting for the person
- */
-export const sayHello = (name) => {
-    return `Hello ${name}`;
-}
+// /**
+//  *
+//  * @param {String} name name of the person
+//  * @returns greeting for the person
+//  */
+// export const sayHello = (name) => {
+//     return `Hello ${name}`;
+// }
+
+// /**
+//  *
+//  * @param {Number} a first number
+//  * @param {Number} b second number
+//  * @returns addition of two numbers
+//  */
+// export const add = (a, b) => {
+//     return a + b;
+// }
 
 /**
- * 
- * @param {Number} a first number
- * @param {Number} b second number
- * @returns addition of two numbers
+ *
+ * @param {Number} index the index of element to be removed
+ * @param {Array} dataList the list from which the element is to be removed
+ * @returns updated array after removing the element
  */
-export const add = (a, b) => {
-    return a + b;
-}
+export const removeItemFromArr = (index, dataList) => {
+  console.log("first", dataList);
+  dataList.splice(index, 1);
+  console.log("next", dataList);
+  return dataList;
+};
